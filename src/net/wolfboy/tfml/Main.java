a530f4e95f9df45a978e8495fa1fe48b9483953e
WolfBoy55014 <bikerduck7@gmail.com>
2023-05-01 17:12:17 -0500

    Python Notification removed

--- src/net/wolfboy/tfml/Main.java
+++ src/net/wolfboy/tfml/Main.java
@@ -7,7 +7,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        System.out.println("This Version has been Modified with Py4J");
 
         SetUpFiles.MakeFiles();
 
